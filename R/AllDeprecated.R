fa51d0b49393224daf45d17410554954705242b8
Oleg Sklyar <osklyar@ebi.ac.uk>
2006-04-26 15:24:36 +0000

    deprecated rearranged
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@17718 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllDeprecated.R
+++ R/AllDeprecated.R
@@ -7,21 +7,32 @@ setClass("Image3D",
     contains = "Image"
 )
 ## ------------------------------------------------------------
-setGeneric("minMax", function(object) standardGeneric("minMax"))
+Image2D <- function(data = array(0, c(1, 1, 1)), dim, rgb = FALSE) {
+    warning("Image2D(...) is deprecated. Use Image(...) instead")
+    Image(data, dim, rgb)
+}
 ## ------------------------------------------------------------
-Image2D <- Image
-Image3D <- Image
+Image3D <- function(data = array(0, c(1, 1, 1)), dim, rgb = FALSE) {
+    warning("Image3D(...) is deprecated. Use Image(...) instead")
+    Image(data, dim, rgb)
+}
 ## ------------------------------------------------------------
 copyImage <- function(x) {
+    warning("copyImage(x) is deprecated. Use copy(...) instead")
     return(copy(x))
 }
 ## ------------------------------------------------------------
+setGeneric("minMax", function(object) standardGeneric("minMax"))
 setMethod("minMax", signature(object = "Image"),
     function(object) {
+        warning("minMax(object) is deprecated. Use range(object) instead")
         if (object@rgb)
             stop("Function supports grayscale images only")
         return(range(object@.Data))
     }
 )
 ## ------------------------------------------------------------
-adaptThresh <- thresh
+adaptThresh <- function(x, width = 20, height = 20, offset = 0.05, preprocess = FALSE) {
+    warning("adaptThresh(...) is deprecated. Use thresh(...) instead. Argument offset has now different range.")
+    thresh(x, width, height, offset, preprocess)
+}
