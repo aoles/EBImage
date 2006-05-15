4abc2b208f451b3bdb267d6b96d1b941dc34ce28
osklyar@ebi.ac.uk <osklyar@ebi.ac.uk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-05-15 14:53:39 +0000

    last EBImage
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@18050 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/class.Drawable.R
+++ R/class.Drawable.R
@@ -1,3 +1,9 @@
+# ============================================================================
+# Drawable etc.: class definitions for different drawable objects
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copyright: Oleg Sklyar, 2006
+#            European Bioinformatics Institute; Bioconductor.org
+# ============================================================================
 setClass("Drawable",
     representation(
         x           = "matrix",
@@ -16,68 +22,85 @@ setClass("Drawable",
         fillOpacity = 0.5
     )
 )
-
-setClass("DrawableText",
-    contains = "Drawable"
-)
-
-DrawableText <- function(text, x = 0, y = 0) {
-    error("to be implemented")
-}
-
+# ============================================================================
+# setClass("DrawableText",
+#     contains = "Drawable"
+# )
+# # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# DrawableText <- function(text, x = 0, y = 0) {
+#     error("to be implemented")
+# }
+# ============================================================================
 setClass("DrawableCircle",
     contains = "Drawable",
     prototype(
-        x           = matrix(0, 3, 1)
+        x           = matrix(0, ncol = 3, nrow = 1)
     ),
-    validity = function(obj) {
-        return(dim(obj@x)[[2]] == 3)
+    validity = function(object) {
+        return(dim(object@x)[[2]] == 3)
     }
 )
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 DrawableCircle <- function(x, y, r) {
     if (missing(x) || missing(y) || missing(r))
         stop("Please supply x-y centre coordinates and radius")
     if (length(x) != length(y) || length(x) != length(r))
         stop("Vectors of coordinates and radius must be of the same length")
-    return(new("DrawableCircle", x = matrix(c(x, y, r), 3, length(x))))
+    return(new("DrawableCircle", x = matrix(c(x, y, r), ncol = 3, nrow = length(x))))
 }
-
+# ============================================================================
 setClass("DrawableLine",
     contains = "Drawable",
-    validity = function(obj) {
-        return(dim(obj@x)[[2]] == 4)
+    prototype(
+        x           = matrix(0, ncol = 4, nrow = 1)
+    ),
+    validity = function(object) {
+        return(dim(object@x)[[2]] == 4)
     }
 )
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 DrawableLine <- function(x1, y1, x2, y2) {
     if (missing(x1) || missing(y1) || missing(x2) || missing(y2))
         stop("Please supply all four coordinates")
     if (length(x1) != length(y1) || length(x2) != length(y2) || length(x1) != length(x2))
         stop("Vectors of coordinates must be of the same length")
-    return(new("DrawableLine", x = matrix(c(x1, y1, x2, y2), 4, length(x1))))
+    return(new("DrawableLine", x = matrix(c(x1, y1, x2, y2), ncol = 4, nrow = length(x1))))
 }
-
+# ============================================================================
 setClass("DrawableRect",
     contains = "DrawableLine"
 )
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 DrawableRect <-function(x1, y1, x2, y2) {
     if (missing(x1) || missing(y1) || missing(x2) || missing(y2))
         stop("Please supply all four coordinates")
     if (length(x1) != length(y1) || length(x2) != length(y2) || length(x1) != length(x2))
         stop("Vectors of coordinates must be of the same length")
-    return(new("DrawableRect", x = matrix(c(x1, y1, x2, y2), 4, length(x1))))
+    return(new("DrawableRect", x = matrix(c(x1, y1, x2, y2), ncol = 4, nrow = length(x1))))
 }
-
+# ============================================================================
 setClass("DrawableEllipse",
-    contains = "DrawableLine"
+    contains = "Drawable",
+    prototype(
+        x           = matrix(0, ncol = 6, nrow = 1)
+    ),
+    validity = function(object) {
+        return(dim(object@x)[[2]] == 6)
+    }
 )
-
-DrawableEllipse <-function(x1, y1, x2, y2) {
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+DrawableEllipse <-function(x1, y1, x2, y2, sgrad = 0, egrad = 360) {
     if (missing(x1) || missing(y1) || missing(x2) || missing(y2))
         stop("Please supply all four coordinates")
     if (length(x1) != length(y1) || length(x2) != length(y2) || length(x1) != length(x2))
         stop("Vectors of coordinates must be of the same length")
-    return(new("DrawableEllipse", x = matrix(c(x1, y1, x2, y2), 4, length(x1))))
+    if (length(sgrad) != length(x1) || length(egrad) != length(x1)) {
+        .sgrad = sgrad[[1]]
+        .egrad = egrad[[1]]
+        sgrad = numeric(length(x1))
+        sgrad[] = .sgrad
+        egrad = numeric(length(x1))
+        egrad[] = egrad[[1]]
+    }
+    return(new("DrawableEllipse", x = matrix(c(x1, y1, x2, y2, sgrad, egrad), ncol = 6, nrow = length(x1))))
 }
