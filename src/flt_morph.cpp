f6e5636117b04aef3a23255eaad8a44879924d5f
osklyar@ebi.ac.uk <osklyar@ebi.ac.uk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-07-05 21:08:00 +0000

    morphological filters started: dev .erode; will error on help - many changes, help not corrected yet
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@18748 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/flt_morph.cpp
@@ -0,0 +1,78 @@
+/* -------------------------------------------------------------------------
+Morphological filters: erode, dilate, open and close
+Copyright (c) 2005 Oleg Sklyar
+See flt_morph.h for license
+------------------------------------------------------------------------- */
+#include "flt_morph.h"
+#include "indexing.h"
+
+#include <R_ext/Error.h>
+#include <iostream>
+
+using namespace std;
+
+inline bool match(int * kernel, double * data, Point & size, Point & at, double & mismatch) {
+    int xx, yy;
+    for (int i = -1; i <= 1; i++)
+        for (int j = -1; j <= 1; j++) {
+            if (!kernel[i + 1 + (j + 1) * 3]) continue;
+            xx = at.x + i;
+            yy = at.y + j;
+            if (xx < 0 || yy < 0 || xx >= size.x || yy >= size.y) return false;
+            if (data[xx + yy * size.x] == mismatch) return false;
+        }
+    return true;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// function modifies its argument !!!  
+SEXP morph_erode(SEXP x, SEXP kernel, SEXP iters) {
+    double resetto = 0; // check foreground
+    try {
+        unsigned int i;
+        if (!assertImage(x))
+            error("wrong argument class, Image expected");
+        if (LOGICAL(GET_SLOT(x, mkString("rgb")))[0])
+            error("input must be a binary image");
+        if (LENGTH(kernel) != 9)
+            error("only 3x3 kernels supported so far");
+        Point size(INTEGER(GET_DIM(x))[0], INTEGER(GET_DIM(x))[1]);
+        unsigned int nimages = INTEGER(GET_DIM(x))[2];
+        int * kern = LOGICAL(kernel);
+        int nrepeats = INTEGER(iters)[0];
+        Point pt;
+        for (i = 0; i < nimages; i++) {
+            double * data = &(REAL(x)[i * size.x * size.y]);
+            for (int it = 0; it < nrepeats; it++) {
+                for (int j = 0; j < size.x * size.y; j++) {
+                    if (data[j] == resetto) continue;
+                    pt = getpoint(j, size.x);    
+                    if (!match(kern, data, size, pt, resetto)) 
+                            data[j] = 0.5;
+                }
+                for (int j = 0; j < size.x * size.y; j++)
+                    if (data[j] == 0.5) data[j] = resetto;
+            }
+        }        
+            
+    }
+    catch(exception &error_) {
+        error(error_.what());
+    }
+    return x;
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+SEXP morph_dilate(SEXP x, SEXP kernel, SEXP iters) {
+
+    return x;
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+SEXP morph_open(SEXP x, SEXP kernel, SEXP iters) {
+
+    return x;
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+SEXP morph_close(SEXP x, SEXP kernel, SEXP iters) {
+
+    return x;
+}
---
+++ src/flt_morph.h
@@ -0,0 +1,30 @@
+/* -------------------------------------------------------------------------
+
+Morphological filters: erode, dilate, open and close
+For binary images
+ 
+Copyright (c) 2006 Oleg Sklyar
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.          
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+GPL license wording: http://www.gnu.org/licenses/gpl.html
+
+------------------------------------------------------------------------- */
+
+#include "common.h"
+
+extern "C" {
+    SEXP morph_erode  (SEXP x, SEXP kernel, SEXP iters);
+    SEXP morph_dilate (SEXP x, SEXP kernel, SEXP iters);
+    SEXP morph_open   (SEXP x, SEXP kernel, SEXP iters);
+    SEXP morph_close  (SEXP x, SEXP kernel, SEXP iters);
+};
+
