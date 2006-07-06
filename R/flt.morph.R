1e044323f5d7ddd136f66d0e97db550253ebe434
osklyar@ebi.ac.uk <osklyar@ebi.ac.uk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-07-06 12:23:52 +0000

    morphological filters support arbitrary kernels!
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@18751 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/flt.morph.R
+++ R/flt.morph.R
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------
-# Morphological filters: erode, dilate, mopen, mclose
+# Morphological filters: mErode, mDilate, mOpen, mClose, mKernel
  
 # Copyright (c) 2006 Oleg Sklyar
 
@@ -16,74 +16,103 @@
 # GPL license wording: http://www.gnu.org/licenses/gpl.html
 
 # -------------------------------------------------------------------------
-.erode <- function(x, kernel = matrix(TRUE,3,3), iter = 2, modify = TRUE) {
+
+mKernel <- function(size = 5, shape = "round") {
+    if (size < 3 || (size / 2 == as.integer(size / 2)))
+        stop("kernel size must be an odd number >= 3: [3, 5, 7, ...")
+    if(switch(shape, round=, square=FALSE, TRUE))
+        stop("available shapes 'round' and 'square'")
+    res <- matrix(as.integer(FALSE), size, size, byrow = TRUE)
+    cx = as.integer(size / 2) + 1
+    if (shape == "round") {
+        res[cx,] = as.integer(TRUE)
+        res[,cx] = as.integer(TRUE)
+        for (i in 1:(cx-1))
+            for (j in 1:(cx-1))
+                if ((cx - i)^2 + (cx - j)^2 <= (cx - 1)^2) {
+                    res[i, j] = as.integer(TRUE)
+                    res[size - i + 1, j] = as.integer(TRUE)
+                    res[i, size - j + 1] = as.integer(TRUE)
+                    res[size - i + 1, size - j + 1] = as.integer(TRUE)
+                }
+        return(res)    
+    }
+
+    # otherwise square
+    res[] = as.integer(TRUE)
+    return(res)
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+.mErode <- function(x, iter = 1, kernel = mKernel(5), modify = TRUE) {
     if (!assert(x))
         stop("Wrong class of argument x, Image expected")
     if (x@rgb)
-        stop("Only grayscale images are supported (only binary at the moment: {0,1})")
+        stop("Only binary images are supported at the moment: {0},{1}")
+    if (!is.integer(kernel) || !is.matrix(kernel))
+        stop("kernel must be an integer matrix")
     if (!modify) {
         x <- copy(x)
         # 0 - erode, 1 - dilate
-        return(.CallEBImage("erodeDilate", x, as.logical(kernel), as.integer(iter), as.integer(0))) 
+        return(.CallEBImage("erodeDilate", x, kernel, as.integer(iter), as.integer(0))) 
     }
     else # original data modified
-        invisible(.CallEBImage("erodeDilate", x, as.logical(kernel), as.integer(iter), as.integer(0))) 
+        invisible(.CallEBImage("erodeDilate", x, kernel, as.integer(iter), as.integer(0))) 
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-erode <- function(x, kernel = matrix(TRUE,3,3), iter = 2) {
-    return(.erode(x, kernel, iter, FALSE))
+mErode <- function(x, iter = 1, kernel = mKernel(5)) {
+    .mErode(x, kernel, iter, FALSE)
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-.dilate <- function(x, kernel = matrix(TRUE,3,3), iter = 2, modify = TRUE) {
+.mDilate <- function(x, iter = 1, kernel = mKernel(5), modify = TRUE) {
     if (!assert(x))
         stop("Wrong class of argument x, Image expected")
     if (x@rgb)
-        stop("Only grayscale images are supported (only binary at the moment: {0,1})")
+        stop("Only binary images are supported at the moment: {0},{1}")
+    if (!is.integer(kernel) || !is.matrix(kernel))
+        stop("kernel must be an integer matrix")
     if (!modify) {
         x <- copy(x)
         # 0 - erode, 1 - dilate
-        return(.CallEBImage("erodeDilate", x, as.logical(kernel), as.integer(iter), as.integer(1))) 
+        return(.CallEBImage("erodeDilate", x, kernel, as.integer(iter), as.integer(1))) 
     }
     else # original data modified
-        invisible(.CallEBImage("erodeDilate", x, as.logical(kernel), as.integer(iter), as.integer(1))) 
+        invisible(.CallEBImage("erodeDilate", x, kernel, as.integer(iter), as.integer(1))) 
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-dilate <- function(x, kernel = matrix(TRUE,3,3), iter = 2) {
-    return(.dilate(x, kernel, iter, FALSE))
+mDilate <- function(x, iter = 1, kernel = mKernel(5)) {
+    .mDilate(x, kernel, iter, FALSE)
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-.mopen <- function(x, kernel = matrix(TRUE,3,3), iter = 2, modify = TRUE) {
+.mOpen <- function(x, iter = 1, kernel = mKernel(5), modify = TRUE) {
     if (!assert(x))
         stop("Wrong class of argument x, Image expected")
     if (x@rgb)
-        stop("Only grayscale images are supported (only binary at the moment: {0,1})")
+        stop("Only binary images are supported at the moment: {0},{1}")
     if (!modify)
         x <- copy(x)
-    .erode(x, kernel, iter, TRUE)
     if (modify)
-        invisible(.dilate(x, kernel, iter, TRUE))
+        invisible(.mDilate(.mErode(x, kernel, iter, TRUE), kernel, iter, TRUE))
     else
-        return(.dilate(x, kernel, iter, TRUE))
+        return(.mDilate(.mErode(x, kernel, iter, TRUE), kernel, iter, TRUE))
 }    
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-mopen <- function(x, kernel = matrix(TRUE,3,3), iter = 2) {
-    return(.mopen(x, kernel, iter, FALSE))
+mOpen <- function(x, iter = 1, kernel = mKernel(5)) {
+    .mOpen(x, kernel, iter, FALSE)
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-.mclose <- function(x, kernel = matrix(TRUE,3,3), iter = 2, modify = TRUE) {
+.mClose <- function(x, iter = 1, kernel = mKernel(5), modify = TRUE) {
     if (!assert(x))
         stop("Wrong class of argument x, Image expected")
     if (x@rgb)
-        stop("Only grayscale images are supported (only binary at the moment: {0,1})")
+        stop("Only binary images are supported at the moment: {0},{1}")
     if (!modify)
         x <- copy(x)
-    .dilate(x, kernel, iter, TRUE)
     if (modify)
-        invisible(.erode(x, kernel, iter, TRUE))
+        invisible(.mErode(.mDilate(x, kernel, iter, TRUE), kernel, iter, TRUE))
     else
-        return(.erode(x, kernel, iter, TRUE))
+        return(.mErode(.mDilate(x, kernel, iter, TRUE), kernel, iter, TRUE))
 }    
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-mclose <- function(x, kernel = matrix(TRUE,3,3), iter = 2) {
-    return(.mclose(x, kernel, iter, FALSE))
+mClose <- function(x, iter = 1, kernel = mKernel(5)) {
+    .mClose(x, kernel, iter, FALSE)
 }
--- inst/scripts/ws.test.R
+++ inst/scripts/ws.test.R
@@ -1,18 +1,23 @@
 # this script will test if the following functions are generally working:
-# read.image, normalize, gaussFilter, thresh, distMap, ws, wsPaint, display, Image
+# read.image, normalize, mOpen, mClose (mErode, mDilate), gaussFilter, thresh, 
+# distMap, ws, wsPaint, display, Image
 
 library(EBImage)
 
-a1 <-normalize(read.image("HT10-C04_A03_w1-0.png"))
-t1 <- thresh(a1, 100, 100, 0.01, TRUE)
+a1 <- read.image("HT10-C04_A03_w1-0.png")
+t1 <- thresh(normalize(a1), 100, 100, 0.01, TRUE)
+t1 <- mClose(mOpen(t1, 1, mKernel(7)), 1, mKernel(7))
+
 dm1 <- sqrt(distMap(t1))
 x1 <- ws(dm1, 15, 10, 0.2, ref = a1)
 img1 <- wsPaint(x1, a1, opac = 0.3)
 
 seeds <- x1$objects[,1:2]
 
-a2 <- sqrt(normalize(a1 + normalize(read.image("HT10-C04_A03_w3-0.png"))))
-t2 <- thresh(a2, 300, 300, 0.00, TRUE)
+a2 <- read.image("HT10-C04_A03_w3-0.png")
+a3 <- sqrt(normalize(a1 + a2))
+t2 <- thresh(a3, 300, 300, 0.00, TRUE)
+t2 <- mClose(mOpen(t2, 1, mKernel(11)), 1, mKernel(11))
 dm2 <- sqrt(distMap(t2))
 x2 <- ws(dm2, 50, 15, 0.2, seeds, ref = a2)
 img2 <- wsPaint(x2, a2, opac = 0.3)
@@ -25,4 +30,9 @@ img3 <- wsPaint(x1, img3, opac = 0.3)
 .dim <- dim(img1)
 .dim[[3]] <- 3
 img <- Image(as.integer(c(img1, img2, img3)), .dim, rgb=TRUE)
-display(img)
+
+nucl <- normalize(wsImages(x1, a1), independent = TRUE)
+cells <- normalize(wsImages(x2, a2), independent = TRUE)
+
+cat("done: display(img), display(nucl), display(cells)\n")
+
