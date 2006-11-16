29e4a7a75401ca68cdcfe97774bc2c0e19a09f2e
osklyar@ebi.ac.uk <osklyar@ebi.ac.uk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-11-16 17:32:43 +0000

    added compilation with GTK+ if abvailable and then automatically using GTK+ display for 2D images
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@21161 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/display.cpp
+++ src/display.cpp
@@ -10,9 +10,7 @@ See: display.h for license
 #include <pthread.h>
 #include <iostream>
 
-
 using namespace std;
-
 
 /* FIXME
    One display at a time is allowed! The problem is that we cannot interrupt
@@ -75,15 +73,56 @@ void * THREAD_FUN_animate(void * ptr) {
     THREAD_ON = false;
     return NULL;
 }
+#ifdef USEGTK
+void mage_gtk_eventHandler(void *userData) {
+    while (gtk_events_pending()) gtk_main_iteration();
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-SEXP displayImages(SEXP rimage, SEXP animate) {
+void mage_gtkDisplay(SEXP rimage) {
+    int argc = 1;
+    typedef char* pxchar;
+    char ** argv = new pxchar[1];
+    argv[0] = "R";
+    if(!gtk_init_check(&argc, &argv))
+        error("Failed to initialize GTK+. Use display(x, TRUE) instead");
+    InputHandler * hdlr = addInputHandler(R_InputHandlers, ConnectionNumber(GDK_DISPLAY()), mage_gtk_eventHandler, -1);
+    MagickImage image = SEXP2Image(rimage);
+    int * dim = INTEGER(GET_DIM(rimage));
+    GdkPixbuf * gdkpx = gdk_pixbuf_new(GDK_COLORSPACE_RGB, true, 8, dim[0], dim[1]);
+    if (image.type() == GrayscaleType)
+        image.write(0, 0, dim[0], dim[1], "IIIA", CharPixel, gdk_pixbuf_get_pixels(gdkpx));
+    else
+        image.write(0, 0, dim[0], dim[1], "RGBA", CharPixel, gdk_pixbuf_get_pixels(gdkpx));
+    GtkWidget * wnd = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_container_set_border_width (GTK_CONTAINER (wnd), 10);
+/*
+    if (NAMED(rimage) > 1)
+        gtk_window_set_title(GTK_WINDOW(wnd), CHAR(PRINTNAME(rimage)));
+    else
+*/
+        gtk_window_set_title(GTK_WINDOW(wnd), "GTK+ R-Image display");
+    GtkWidget * img = gtk_image_new_from_pixbuf(gdkpx);
+    g_object_unref(gdkpx);
+    gtk_container_add(GTK_CONTAINER (wnd), img);
+    gtk_widget_show_all(wnd);
+    gdk_flush();
+
+    // FIXME -- check if I need finalizers for this window, probably not, but check
+}
+#endif
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+SEXP displayImages(SEXP rimage, SEXP nogtk) {
     if (!assertImage(rimage))
         error("Wrong argument class, Image expected");
     try {
-        bool doanimate = LOGICAL(animate)[0];
         /* test dimensionality: 2D and 3D only can be displayed */
         /* FIXME This must be uncommented to force closing image before opening a new one!!!
         */
+#ifdef USEGTK
+        if (!LOGICAL(nogtk)[0] && assertImage2D(rimage))
+            mage_gtkDisplay(rimage);
+        else {
+#endif
         if (THREAD_ON)
             error("Close currently displayed image first");
         pthread_t res;
@@ -92,14 +131,12 @@ SEXP displayImages(SEXP rimage, SEXP animate) {
                 error("Cannot create display thread");
         }
         else {
-            if (doanimate) {
-                if (pthread_create(&res, NULL, THREAD_FUN_animate, (void *)rimage) != 0)
-                    error("Cannot create display thread");
-            }
-            else
-                if (pthread_create(&res, NULL, THREAD_FUN_display, (void *)rimage) != 0)
-                    error("Cannot create display thread");
+            if (pthread_create(&res, NULL, THREAD_FUN_display, (void *)rimage) != 0)
+                error("Cannot create display thread");
         }
+#ifdef USEGTK
+        }
+#endif
     }
     catch(exception &error_) {
         error(error_.what());
--- src/display.h
+++ src/display.h
@@ -1,26 +1,31 @@
 /* -------------------------------------------------------------------------
 
 Image display
- 
+
 Copyright (c) 2005 Oleg Sklyar
 
 This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public License 
+modify it under the terms of the GNU Lesser General Public License
 as published by the Free Software Foundation; either version 2.1
-of the License, or (at your option) any later version.          
+of the License, or (at your option) any later version.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 See the GNU Lesser General Public License for more details.
 LGPL license wording: http://www.gnu.org/licenses/lgpl.html
 
 ------------------------------------------------------------------------- */
+#ifdef USEGTK
+    #include <gtk/gtk.h>
+    #include "R_ext/eventloop.h"
+    #include <gdk/gdkx.h>
+#endif
 
 #include "common.h"
 
 extern "C" {
-    SEXP displayImages(SEXP rimage, SEXP animate);
+    SEXP displayImages(SEXP rimage, SEXP nogtk);
 };
 
