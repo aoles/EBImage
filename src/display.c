2bae1de40d0875f1ae2d0646a1f6d8d4a9319c24
Gregoire Pau <gregoire.pau@gmail.com>
2009-06-26 09:20:03 +0000

    fix noGTK support in display.c
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@40328 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/display.c
+++ src/display.c
@@ -34,9 +34,9 @@ typedef struct {
   double nx,ny,nz;
   double x,y;
   double zoom;
-  GtkWidget *hSlider;
-  GtkWidget *imgWG;
-  GtkWidget *stbarWG;
+  void *hSlider;
+  void *imgWG;
+  void *stbarWG;
   int index;
   SEXP xx;
 } udata;
