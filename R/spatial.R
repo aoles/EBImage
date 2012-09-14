500b61778d9f37369622cc25f76a8c64953103b4
Andrzej Oleś <andrzej.oles@gmail.com>
2012-09-14 23:50:08 +0000

    * fixes to readImage2/writeImage2 functions along with the modified swapXY function + in /inst/scripts/: scripts for testing the new read/write functions against the former ones using test images from the on-line EBImage Test Images repository (http://www-huber.embl.de/EBImage/ExampleImages/)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@69461 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/spatial.R
+++ R/spatial.R
@@ -90,9 +90,17 @@ resize <- function(x, w, h, filter="bilinear", output.dim, output.origin=c(0, 0)
   affine(x, m, filter, output.dim=output.dim)
 }
 
-## swap the XY dimensions
-swapXY = function(x) {
-	dims = 1:length(dim(x))
-	dims[1:2] = c(2:1)
-	aperm(x, dims)
+## swaps the XY dimensions
+## AO: note on performance: when a permutation of imageData rather than an Image object is needed as output it is much faster to issue the function directly on the Image object with 'keepClass = FALSE' than to extract the imageData first ond only then perform the swapping
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+swapXY = function(x, keepClass = TRUE) {
+  validImage(x)
+  dims = 1:length(dim(x))
+  dims[1:2] = c(2:1)
+  y = aperm(x, dims)
+  if ( keepClass && is.Image(x) )
+    x@.Data = y
+  else 
+    x = y
+  x
 }
