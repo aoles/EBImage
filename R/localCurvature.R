0ef14bc9e749e65321aea40801d33e4593cb65f0
j.barry <j.barry@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-02-11 17:31:00 +0000

    minor edits to function and man page of localCurvature
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@73355 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/localCurvature.R
+++ R/localCurvature.R
@@ -1,9 +1,9 @@
 localCurvature = function(x, h=100, maxk=10000) {
- df=as.data.frame(x)
- colnames(df)=c('x', 'y')
- px = c(0, cumsum(sqrt(c(rowSums(sapply(df, diff)^2)))))
+ # check that x has proper dimensions
+ if (length(dim(x)) != 2 | dim(x)[2] !=2) stop("'x' should have dimensions of N x 2")
+ px = c(0, cumsum(sqrt(rowSums(diff(x)^2))))
  poly = lp(px, deg=2, h=h)
- derivatives = apply(df, 2, function(v) {
+ derivatives = apply(x, 2, function(v) {
   d1 = locfit(v ~ poly, deriv=1, maxk=maxk)
   d2 = locfit(v ~ poly, deriv=c(1,1), maxk=maxk)
   list(predict(d1, newdata=data.frame(px=px)),
