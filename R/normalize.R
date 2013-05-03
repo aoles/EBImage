d860e84b81d8d9f47ba5aa5e020ab5a18120a866
a.oles <a.oles@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-05-03 15:50:15 +0000

    'normalize' now uses the generic from BiocGenerics
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@76233 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalize.R
+++ R/normalize.R
@@ -1,8 +1,12 @@
-normalize <- function (x, separate=TRUE, ft=c(0, 1)) {
-  validImage(x)
+normalize.Image = function(object, separate = TRUE, ft = c(0, 1)) {
+  validImage(object)
   ft <- as.numeric (ft)
   if ( diff(ft) == 0 ) stop("normalization range is 0")
   separate <- as.integer(separate)
-  x = .Call("normalize", castImage(x), separate, ft, PACKAGE='EBImage')
-  return(x)
+  object = .Call("normalize", castImage(object), separate, ft, PACKAGE='EBImage')
+  return(object)
 }
+
+setMethod("normalize", signature("Image"), normalize.Image)
+setMethod("normalize", signature("array"), normalize.Image)
+setMethod("normalize", signature("matrix"), normalize.Image)
