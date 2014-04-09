b408df7a433c2ec3e1c5c205e3f873aa644fa4c2
a.oles <a.oles@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-04-09 15:56:01 +0000

    version 4.5.20 - added grid lines to 'displayRaster' - cleaned up 'displayRaster' code
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@88661 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/display.R
+++ R/display.R
@@ -12,7 +12,60 @@ display = function(x, title = deparse(substitute(x), width.cutoff = 500L, nlines
 
 ## displays an image using R graphics
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-displayRaster = function(image, frame, all = FALSE, interpolate = TRUE){
+displayRaster = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
+  all = isTRUE(all)
+  nf = getNumberOfFrames(image, type="render")
+  
+  ## display all frames in a grid-like environment
+  if ( all ) {
+    ncol = ceiling(sqrt(nf))
+    nrow = ceiling(nf/ncol)
+  }
+  ## display a single frame only
+  else {
+    ## when the image contains a single frame only display it and don't care about the 'frame' argument at all
+    if (nf==1)
+      frame = 1
+    else 
+      if (missing(frame)) {
+        frame = 1
+        message("The image contains more than one frame: only the first one is displayed. To display all frames use 'all = TRUE'.")
+      }
+    else
+      if ( frame<1 || frame>nf ) stop("Incorrect 'frame' number: It must range between 1 and ", frame)
+    
+    ncol = nrow = 1
+  }
+    
+  dim <- dim(image) 
+  xdim <- dim[1]
+  ydim <- dim[2]
+  
+  xran = c(0, ncol*xdim) + .5
+  yran = c(0, nrow*ydim) + .5
+  
+  ## set graphical parameters
+  par(bty="n", mai=c(0,0,0,0), xaxs="i", yaxs="i", xaxt="n", yaxt="n")    
+  plot(xran, yran, type="n", xlab="", ylab="", asp=1, ylim=rev(yran))
+  
+  for(r in seq_len(nrow)) {
+    for(c in seq_len(ncol)) {
+      f = if(all) (r-1)*ncol + c else frame
+      if ( f <= nf )
+        rasterImage(getFrame(image, f, type="render"), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, ...)
+      else
+        break
+    }
+  }    
+    
+  ## draw grid lines in case of multiple frames
+  if( all && isTRUE(drawGrid) && nf>1 ) {
+    clip(xran[1], xran[2], yran[1], yran[2])
+    abline(h = seq_len(ncol-1)*xdim + .5, v = seq_len(ncol-1)*xdim + .5, col = par()$bg)
+  }
+}
+
+displayRaster.old = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
 
   nf = getNumberOfFrames(image, type="render")
 
@@ -38,11 +91,16 @@ displayRaster = function(image, frame, all = FALSE, interpolate = TRUE){
       for(c in seq_len(ncol)) {
         ## plot the figure as a raster image
         if (f<=nf) {
-          rasterImage(getFrame(image, f, type="render"), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, interpolate = interpolate)
+          rasterImage(getFrame(image, f, type="render"), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, ...)
           f = f + 1
         }
         else break
       }
+    }    
+    
+    if(isTRUE(drawGrid)){
+      clip(xran[1], xran[2], yran[1], yran[2])
+      abline(h = seq_len(ncol-1)*xdim + .5, v = seq_len(ncol-1)*xdim + .5, col = par()$bg)
     }
   }
 
@@ -65,8 +123,9 @@ displayRaster = function(image, frame, all = FALSE, interpolate = TRUE){
     plot(xran, yran, type="n", xlab="", ylab="", asp=1, ylim=rev(yran))
 
     ## plot the figure as a raster image
-    rasterImage(getFrame(image, frame, type="render"), xran[1], yran[2], xran[2], yran[1], interpolate = interpolate)
+    rasterImage(getFrame(image, frame, type="render"), xran[1], yran[2], xran[2], yran[1], ...)
    }
+  
 }
 
 ## displays an image using JavaScript
