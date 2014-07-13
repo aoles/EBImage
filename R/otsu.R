4b72ecce081ce290f2c860f0f15e6e1a22ee3625
a.oles <a.oles@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-07-13 17:27:21 +0000

    version 4.7.14 - improvements and fixes to the 'otsu' method
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@92463 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/otsu.R
+++ R/otsu.R
@@ -1,24 +1,29 @@
 
-otsu <- function(x, levels = 256){
+otsu <- function(x, range = c(0, 1), levels = 256){
   validImage(x)
-  if (colorMode(x) != Grayscale) stop("Only thresholding of Grayscale images is supported.")
+  if ( colorMode(x) != Grayscale ) stop("Only thresholding of Grayscale images is supported.")
+  if ( !is.numeric(range) || length(range) != 2 ) stop("'range' must be a numeric vector of length 2.")
   levels = as.integer(levels)
-  if (levels < 2 ) stop("Levels must be at least equal 2.")
+  if ( is.na(levels) || levels < 1 ) stop("Levels must be at least equal 1.") 
+  breaks = seq(range[1], range[2], length.out = levels+1)
   
 # prepare 3D array for apply function 
   dim(x) = c(dim(x)[seq_len(2)], getNumberOfFrames(x, 'total'))
   
 # threshold each frame separately
   apply(x, 3, function(y) {
-    h = hist.default(y, breaks = seq(0, 1, length.out = levels+1), plot = FALSE)
+    h = hist.default(y, breaks = breaks, plot = FALSE)
     counts = as.double(h$counts)
     mids = as.double(h$mids)
     len = length(counts)
     w1 = cumsum(counts)
-    w2 = w1[len] - w1
-    m1 = cumsum(counts * mids)
-    m2 = m1[len] - m1
+    w2 = w1[len] + counts - w1
+    cm = counts * mids
+    m1 = cumsum(cm)
+    m2 = m1[len] + cm - m1
     var = w1 * w2 * (m2/w2 - m1/w1)^2
-    mids[which.max(var)]
+    # find the left- and right-most maximum and return the threshold value in between
+    maxi = which(var == max(var, na.rm = TRUE))
+    (mids[maxi[1]] + mids[maxi[length(maxi)]] ) /2
   })
 }
