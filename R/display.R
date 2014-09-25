235cad7dfc33f9c79526c2cc7fe98cd13f0d5ba7
Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
2014-09-25 09:35:47 -0700

    Commit made by the Bioconductor Git-SVN bridge.
    Consists of 1 commit.
    
    Commit information:
    
    SVN Revision number: 94530
    Commit message:
    version 4.7.18
    - improved performance of 'displayRaster'
    - improved performance of 'validImageObject' (2x)
    
    Committed by a.oles
    Committed at: 2014-09-25T16:34:36.350515Z

--- R/display.R
+++ R/display.R
@@ -5,7 +5,7 @@ display = function(x, title = deparse(substitute(x), width.cutoff = 500L, nlines
 
   switch(method,
     browser = displayInBrowser(x, title),
-    raster  = displayRaster.native(x, frame, all) ) 
+    raster  = displayRaster(x, frame, all) ) 
 
   invisible(NULL)
 }
@@ -14,7 +14,7 @@ display = function(x, title = deparse(substitute(x), width.cutoff = 500L, nlines
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 displayRaster = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
   all = isTRUE(all)
-  nf = numberOfFrames(image, type="render")
+  nf = .numberOfFrames(image, type="render")
   
   ## display all frames in a grid-like environment
   if ( all ) {
@@ -32,11 +32,11 @@ displayRaster = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
         message("The image contains more than one frame: only the first one is displayed. To display all frames use 'all = TRUE'.")
       }
     else
-      if ( frame<1 || frame>nf ) stop("Incorrect 'frame' number: It must range between 1 and ", frame)
+      if ( frame<1 || frame>nf ) stop("Incorrect number of frame: it must range between 1 and ", frame)
     
     ncol = nrow = 1L
   }
-    
+  
   dim <- dim(image) 
   xdim <- dim[1]
   ydim <- dim[2]
@@ -48,16 +48,19 @@ displayRaster = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
   par(bty="n", mai=c(0,0,0,0), xaxs="i", yaxs="i", xaxt="n", yaxt="n")    
   plot(xran, yran, type="n", xlab="", ylab="", asp=1, ylim=rev(yran))
   
+  colormode = colorMode(image)
+  img = imageData(image)
+  
   for(r in seq_len(nrow)) {
     for(c in seq_len(ncol)) {
       f = if(all) (r-1)*ncol + c else frame
       if ( f <= nf )
-        rasterImage(getFrame(image, f, type="render"), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, ...)
+        rasterImage(as.nativeRaster(.getFrame(img, f, type="render", colormode = colormode)), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, ...)
       else
         break
     }
   }    
-    
+  
   ## draw grid lines in case of multiple frames
   if( all && isTRUE(drawGrid) && nf>1 ) {
     clip(xran[1], xran[2], yran[1], yran[2])
@@ -65,58 +68,6 @@ displayRaster = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
   }
 }
 
-displayRaster.native = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
-  all = isTRUE(all)
-  nf = numberOfFrames(image, type="render")
-  
-  ## display all frames in a grid-like environment
-  if ( all ) {
-    ncol = ceiling(sqrt(nf))
-    nrow = ceiling(nf/ncol)
-  }
-  ## display a single frame only
-  else {
-    ## when the image contains a single frame only display it and don't care about the 'frame' argument at all
-    if (nf==1)
-      frame = 1
-    else 
-      if (missing(frame)) {
-        frame = 1
-        message("The image contains more than one frame: only the first one is displayed. To display all frames use 'all = TRUE'.")
-      }
-    else
-      if ( frame<1 || frame>nf ) stop("Incorrect 'frame' number: It must range between 1 and ", frame)
-    
-    ncol = nrow = 1L
-  }
-  
-  dim <- dim(image) 
-  xdim <- dim[1]
-  ydim <- dim[2]
-  
-  xran = c(0, ncol*xdim) + .5
-  yran = c(0, nrow*ydim) + .5
-  
-  ## set graphical parameters
-  par(bty="n", mai=c(0,0,0,0), xaxs="i", yaxs="i", xaxt="n", yaxt="n")    
-  plot(xran, yran, type="n", xlab="", ylab="", asp=1, ylim=rev(yran))
-  
-  for(r in seq_len(nrow)) {
-    for(c in seq_len(ncol)) {
-      f = if(all) (r-1)*ncol + c else frame
-      if ( f <= nf )
-        rasterImage(as.nativeRaster(getFrame(image, f, type="render")), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, ...)
-      else
-        break
-    }
-  }    
-  
-  ## draw grid lines in case of multiple frames
-  if( all && isTRUE(drawGrid) && nf>1 ) {
-    clip(xran[1], xran[2], yran[1], yran[2])
-    abline(h = seq_len(nrow-1)*ydim + .5, v = seq_len(ncol-1)*xdim + .5, col = "white")
-  }
-}
 ## displays an image using JavaScript
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 displayInBrowser = function(x, title){
@@ -138,7 +89,7 @@ displayInBrowser = function(x, title){
 
   ## get image parameters
   dims = dim(x)
-  nf = numberOfFrames(x, "render")
+  nf = .numberOfFrames(x, "render")
 
   ## fill-in in the template
   a = sub("HEIGHT",dims[2], sub("WIDTH",dims[1], sub("FRAMES",nf, sub("IMAGE",basename(imageFile), sub("TITLE", title, a)))))
@@ -157,8 +108,10 @@ displayInBrowser = function(x, title){
   browseURL(query)
 }
 
+## take plain array as input
+
 as.nativeRaster = function(x) {
-  x = round(imageData(x) * 255)
+  x = round(x * 255)
   
   y = if(length(dim(x)) == 3L)
     x[,,1L] + x[,,2L] * 256 + x[,,3L] * 65536 + 255 * 16777216 - 4294967296
