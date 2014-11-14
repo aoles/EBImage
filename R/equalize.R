e31fa27a82b494222fd74a4df64a3aedb46c6dcf
Andrzej Oleś <andrzej.oles@gmail.com>
2014-11-14 10:08:55 +0000

    fix to previous commit
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@96754 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/equalize.R
+++ R/equalize.R
@@ -1,7 +1,6 @@
 
 equalize <- function(x, range = c(0, 1), levels = 256){
   validImage(x)
-  #if ( colorMode(x) != Grayscale ) stop("Only thresholding of Grayscale images is supported.")
   if ( !is.numeric(range) || length(range) != 2 ) stop("'range' must be a numeric vector of length 2.")
   levels = as.integer(levels)
   if ( is.na(levels) || levels < 1 ) stop("Levels must be at least equal 1.") 
@@ -10,17 +9,20 @@ equalize <- function(x, range = c(0, 1), levels = 256){
   d = dim(x)
   n = prod(d[seq_len(2)])
   
-# threshold each frame separately
-  res = apply(x, 3:length(dim(x)), function(y) {
+# equalize each frame separately
+  .equalize = function(y) {
     h = hist.default(y, breaks = breaks, plot = FALSE)
     cdf = cumsum(h$counts)
     cdf_min = min(cdf[cdf>0])
     
     equalized = ( (cdf - cdf_min) / (prod(dim(y)) - cdf_min) * (range[2] - range[1]) ) + range[1]
     bins = round( (y - range[1]) / (range[2] - range[1]) * (levels-1) ) + 1
-
+    
     res = equalized[bins]
-  })
+  }
+
+  res = if ( (ld = length(d)) > 2L ) apply(x, 3L:ld, .equalize)
+        else .equalize(x)
 
 # setup output image
   dim(res) = dim(x)
