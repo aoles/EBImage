3c2eec1aeb221e72e969a6f9b3db0e47b1ac957b
Andrzej Oleś <andrzej.oles@gmail.com>
2015-03-23 11:03:15 +0000

    version 4.9.18 - refactored 'paintObjects': allow control over behaviour at image edges
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@101019 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/objects.R
+++ R/objects.R
@@ -13,21 +13,32 @@
 # LGPL license wording: http://www.gnu.org/licenses/lgpl.html
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-paintObjects = function (x, tgt, opac=c(1, 1), col=c('red', NA), thick=FALSE) {
+paintObjects = function (x, tgt, opac=c(1, 1), col=c('red', NA), thick=FALSE, closed=FALSE) {
   validImage(x)
-  if (colorMode(x)!=Grayscale)  stop("'", deparse(substitute(x), width.cutoff = 500L, nlines = 1), "' must be in 'Grayscale' color mode")
+  if (colorMode(x)!=Grayscale) stop("'", deparse(substitute(x), width.cutoff = 500L, nlines = 1L), "' must be in 'Grayscale' color mode")
   if (any(dim(x)[1:2] != dim(tgt)[1:2])) stop( "'x' and 'tgt' must have the same size" )
   if (.numberOfFrames(x,'render') != .numberOfFrames(tgt,'render')) stop( "'x' and 'tgt' must have the same number of render frames" )                           
   
-  col = c(col, rep(NA, 3-length(col)))
-  opac = c(opac, rep(1, 3-length(opac)))
-  zcol = which(is.na(col))
+  if ((l=length(col))>2L) col[1:2] else col = switch(l+1L, c(NA, NA), c(col, NA), col)
+  if ((l=length(opac))>2L) opac[1:2] else opac = switch(l+1L, c(1, 1), c(opac, 1), opac)
+  
+  zcol = is.na(col)
   col[zcol] = 'white'
   opac[zcol] = 0
   
   opac = as.numeric(opac)
   if (any(opac < 0) || any(opac > 1)) stop("all opacity values must be in the range [0,1]" )
   
+  ## behavior at image borders
+  if ( isTRUE(closed) ) {
+    col[3L] = col[1L] 
+    opac[3L] = opac[1L]
+  }
+  else {
+    col[3L] = col[2L] 
+    opac[3L] = opac[2L]
+  }
+    
   .Call(C_paintObjects, castImage(x), castImage(tgt), opac, Image(col, colormode = colorMode(tgt)), as.integer(thick))
 }
 
