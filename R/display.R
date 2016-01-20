5741d0aeeee389c959bdda7f2879d6468f984911
a.oles <a.oles@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-01-20 11:31:06 +0000

    version 4.13.6   - fix 'as.raster.Image' method: increased performance by not performing the transposition twice and improved support for Color images with less than 3 channels   - re-generate 'test.Rout.save' to reflect recent updates
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/EBImage@112736 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/display.R
+++ R/display.R
@@ -66,64 +66,6 @@ displayRaster = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
   }
 }
 
-# nocov start
-displayRaster.old = function(image, frame, all = FALSE, drawGrid = TRUE, ...){
-  all = isTRUE(all)
-  nf = .numberOfFrames(image, type="render")
-  
-  ## display all frames in a grid-like environment
-  if ( all ) {
-    ncol = ceiling(sqrt(nf))
-    nrow = ceiling(nf/ncol)
-  }
-  ## display a single frame only
-  else {
-    ## when the image contains a single frame only display it and don't care about the 'frame' argument at all
-    if (nf==1)
-      frame = 1
-    else 
-      if (missing(frame)) {
-        frame = 1
-        message("The image contains more than one frame: only the first one is displayed. To display all frames use 'all = TRUE'.")
-      }
-    else
-      if ( frame<1 || frame>nf ) stop("Incorrect number of frame: it must range between 1 and ", frame)
-    
-    ncol = nrow = 1L
-  }
-  
-  dim <- dim(image) 
-  xdim <- dim[1]
-  ydim <- dim[2]
-  
-  xran = c(0, ncol*xdim) + .5
-  yran = c(0, nrow*ydim) + .5
-  
-  ## set graphical parameters
-  par(bty="n", mai=c(0,0,0,0), xaxs="i", yaxs="i", xaxt="n", yaxt="n")    
-  plot(xran, yran, type="n", xlab="", ylab="", asp=1, ylim=rev(yran))
-  
-  colormode = colorMode(image)
-  img = imageData(image)
-  
-  for(r in seq_len(nrow)) {
-    for(c in seq_len(ncol)) {
-      f = if(all) (r-1)*ncol + c else frame
-      if ( f <= nf )
-        rasterImage(as.nativeRaster.old(.getFrame(img, f, type="render", colormode = colormode)), (c-1)*xdim + .5, r*ydim + .5, c*xdim + .5, (r-1)*ydim +.5, ...)
-      else
-        break
-    }
-  }    
-  
-  ## draw grid lines in case of multiple frames
-  if( all && isTRUE(drawGrid) && nf>1 ) {
-    clip(xran[1], xran[2], yran[1], yran[2])
-    abline(h = seq_len(nrow-1)*ydim + .5, v = seq_len(ncol-1)*xdim + .5, col = "white")
-  }
-}
-# nocov end
-
 ## displays an image using JavaScript
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 displayInBrowser = function(x, title, ...){
@@ -173,25 +115,4 @@ displayInBrowser = function(x, title, ...){
   browseURL(query, ...)
 }
 
-# nocov start
-# take plain array as input
-as.nativeRaster.old = function(x) {
-  x = clipImage(x)
-  x = round(x * 255)
-  d = dim(x)
-  
-  y = if(length(d) == 3L)
-    Reduce("+", lapply(seq_len(4), function(i) 2^((i-1)*8) * if (i > d[3L]) { if ( i == 4L) 255 else 0 } else x[,,i]) ) - 4294967296
-  else
-    x + x * 256 + x * 65536 + 255 * 16777216 - 4294967296
-  storage.mode(y) = "integer"
-  
-  # native raster representation contains already transposed image data as in the Image class, but with swapped dimensions!
-  attr(y, "dim") = dim(y)[2:1]
-  attr(y, "class") = "nativeRaster"
-  attr(y, "channels") = 4L
-  y
-}
-# nocov end
-
-as.nativeRaster = function(x) .Call(C_nativeRaster, castImage(x))  
+as.nativeRaster = function(x) .Call(C_nativeRaster, castImage(x))
